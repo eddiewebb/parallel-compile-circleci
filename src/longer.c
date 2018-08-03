a585732c325894b763d2091d0515f189469a6c5e
Eddie Webb <ollitech@gmail.com>
2018-08-03 14:04:33 -0400

    add longer file

--- src/longer.c
+++ src/longer.c
@@ -32,3 +32,49 @@ int longer() {
         printf("%d is not a leap year.", year);
    return 0;
 }
+
+int fib()
+{
+    int t1 = 0, t2 = 1, nextTerm = 0, n;
+
+    printf("Enter a positive number: ");
+    scanf("%d", &n);
+
+    // displays the first two terms which is always 0 and 1
+    printf("Fibonacci Series: %d, %d, ", t1, t2);
+
+    nextTerm = t1 + t2;
+
+    while(nextTerm <= n)
+    {
+        printf("%d, ",nextTerm);
+        t1 = t2;
+        t2 = nextTerm;
+        nextTerm = t1 + t2;
+    }
+    
+    return 0;
+}
+
+int prime()
+{
+    int base, exponent;
+
+    long long result = 1;
+
+    printf("Enter a base number: ");
+    scanf("%d", &base);
+
+    printf("Enter an exponent: ");
+    scanf("%d", &exponent);
+
+    while (exponent != 0)
+    {
+        result *= base;
+        --exponent;
+    }
+
+    printf("Answer = %lld", result);
+
+    return 0;
+}
