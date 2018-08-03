f02d2836aff21c296eeca1c89b989e72a3ecacfb
Eddie Webb <ollitech@gmail.com>
2018-08-03 14:02:11 -0400

    add longer file

--- src/longer.c
+++ src/longer.c
@@ -1,118 +1,34 @@
 #include <stdio.h>
  
 int longer() {
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
-   printf("Until next time!\n");
+    double firstNumber, secondNumber, product;
+    printf("Enter two numbers: ");
+    // Stores two floating point numbers in variable firstNumber and secondNumber respectively
+    scanf("%lf %lf", &firstNumber, &secondNumber);  
+    // Performs multiplication and stores the result in variable productOfTwoNumbers
+    product = firstNumber * secondNumber;  
+    // Result up to 2 decimal point is displayed using %.2lf
+    printf("Product = %.2lf", product);
+
+
+
+    int year;
+    printf("Enter a year: ");
+    scanf("%d",&year);
+    if(year%4 == 0)
+    {
+        if( year%100 == 0)
+        {
+            // year is divisible by 400, hence the year is a leap year
+            if ( year%400 == 0)
+                printf("%d is a leap year.", year);
+            else
+                printf("%d is not a leap year.", year);
+        }
+        else
+            printf("%d is a leap year.", year );
+    }
+    else
+        printf("%d is not a leap year.", year);
    return 0;
 }
